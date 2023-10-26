dfb7ee840569f582daf9e2b5c25a677cd1468b23
praneetha <user627@host.docker.internal>
2023-10-26 12:24:39 +0530

    commit new Controller method

---
+++ devops/src/main/java/com/example/demo/trainer/TrainerController.java
@@ -0,0 +1,12 @@
+package com.example.demo.trainer;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TrainerController {
+	
+	public String sendMsg() {
+		return "Hello";
+	}
+
+}
