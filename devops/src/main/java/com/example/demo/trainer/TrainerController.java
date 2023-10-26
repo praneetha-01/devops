84f2fe823d6fa4fc4c1f57ce64ca008674abb271
praneetha <user627@host.docker.internal>
2023-10-26 12:32:42 +0530

    commit changes made to trainer controller class

--- devops/src/main/java/com/example/demo/trainer/TrainerController.java
+++ devops/src/main/java/com/example/demo/trainer/TrainerController.java
@@ -1,12 +1,14 @@
 package com.example.demo.trainer;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrainerController {
 	
+	@GetMapping(path="/bye")
 	public String sendMsg() {
-		return "Hello";
+		return "Hello from Praneetha";
 	}
 
 }
